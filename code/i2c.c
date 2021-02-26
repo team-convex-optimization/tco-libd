1e9d4fbeb3f7ec65658c7125656dddd461a1397d
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-02-26 18:37:39 +0100

    Update the I2C interface to allow for more flexibility

--- code/i2c.c
+++ code/i2c.c
@@ -11,12 +11,12 @@
 
 #include "tco_libd.h"
 
-int i2c_port_open(uint8_t interface_id)
+i2c_error_t i2c_port_open(uint8_t const interface_id, int *fd_loc)
 {
     if (interface_id > 9)
     {
         log_error("I2C adapter IDs greater than 9 are not supported");
-        return -1;
+        return ERR_CRIT;
     }
     char id_str = '0' + interface_id;
     char path[11] = "/dev/i2c- \0";
@@ -26,12 +26,13 @@ int i2c_port_open(uint8_t interface_id)
     {
         log_error("open: %s", strerror(errno));
         log_error("Tried to open '%s'", path);
-        return -1;
+        return ERR_CRIT;
     }
-    return fd;
+    *fd_loc = fd;
+    return ERR_CRIT;
 }
 
-i2c_error_t i2c_cmd_write(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_t data, uint8_t data_send)
+i2c_error_t i2c_cmd_write(int const i2c_port_fd, uint8_t const i2c_addr, uint8_t const cmd, uint8_t *const data, uint8_t const data_size)
 {
     if (ioctl(i2c_port_fd, I2C_SLAVE, i2c_addr) < 0)
     {
@@ -39,18 +40,15 @@ i2c_error_t i2c_cmd_write(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_
         log_error("ioctl: %s", strerror(errno));
         return ERR_I2C_CTRL;
     }
-    if (data_send)
+    if (i2c_smbus_write_byte(i2c_port_fd, cmd) != 0)
     {
-        if (i2c_smbus_write_byte_data(i2c_port_fd, cmd, data) != 0)
-        {
-            log_error("Failed to send command and data");
-            log_error("i2c_smbus_write_byte_data: %s", strerror(errno));
-            return ERR_I2C_WRITE;
-        }
+        log_error("Failed to send command");
+        log_error("i2c_smbus_write_byte: %s", strerror(errno));
+        return ERR_I2C_WRITE;
     }
-    else
+    for (uint8_t data_idx = 0; data_idx < data_size; data_idx++)
     {
-        if (i2c_smbus_write_byte(i2c_port_fd, cmd) != 0)
+        if (i2c_smbus_write_byte(i2c_port_fd, data[data_idx]) != 0)
         {
             log_error("Failed to send command");
             log_error("i2c_smbus_write_byte: %s", strerror(errno));
@@ -61,7 +59,7 @@ i2c_error_t i2c_cmd_write(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_
     return ERR_OK;
 }
 
-i2c_error_t i2c_cmd_read(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_t *output)
+i2c_error_t i2c_cmd_read(int const i2c_port_fd, uint8_t const i2c_addr, uint8_t const cmd, uint8_t *const output, uint8_t const output_size)
 {
     if (ioctl(i2c_port_fd, I2C_SLAVE, i2c_addr) < 0)
     {
@@ -69,13 +67,23 @@ i2c_error_t i2c_cmd_read(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_t
         log_error("ioctl: %s", strerror(errno));
         return ERR_I2C_CTRL;
     }
-    int const output_tmp = i2c_smbus_read_byte_data(i2c_port_fd, cmd);
-    if (output_tmp < 0)
+
+    if (i2c_smbus_write_byte(i2c_port_fd, cmd) != 0)
     {
-        log_error("Failed to send command and read the response");
-        log_error("i2c_smbus_read_byte: %s", strerror(errno));
-        return ERR_I2C_READ;
+        log_error("Failed to send command");
+        log_error("i2c_smbus_write_byte: %s", strerror(errno));
+        return ERR_I2C_WRITE;
     }
-    *output = output_tmp;
+    for (uint8_t byte_idx = 0; byte_idx < output_size; byte_idx++)
+    {
+        int const output_tmp = i2c_smbus_read(i2c_port_fd);
+        if (output_tmp < 0)
+        {
+            log_error("Failed to read byte number %i", byte_idx);
+            log_error("i2c_smbus_read: %s", strerror(errno));
+            return ERR_I2C_READ;
+        }
+        output[byte_idx] = output_tmp;
+    }
     return ERR_OK;
 }
--- include/tco_libd_i2c.h
+++ include/tco_libd_i2c.h
@@ -17,27 +17,31 @@ typedef enum i2c_error_t
 /**
  * @brief Open an I2C port.
  * @param interface_id  The interface to open. E.g. 1 means i2c-1 will be opened (if it exists).
- * @return File descriptor of the opened port on success, -1 on failure.
+ * @param fd_loc Location where the file descriptor will be written.
+ * @return Status code.
  */
-int i2c_port_open(uint8_t interface_id);
+i2c_error_t i2c_port_open(uint8_t const interface_id, int *fd_loc);
 
 /**
  * @brief Send a command with data. E.g. Send a register address, and write to its contents.
  * @param i2c_port_fd File descriptor of the i2c port.
  * @param cmd The byte that will be interpreted by the slave.
- * @param data The data that will be sent to the slave.
- * @param data_send When >0 data will be sent after command, when =0 data will be ignored.
- * @return Status code
+ * @param data Pointer to the data that will be sent to the slave. If there is no data, NULL is
+ * accepted.
+ * @param data_size Indicates the number of bytes that will be read from "data" and sent to the
+ * slave in a row after sending the command.
+ * @return Status code.
  */
-i2c_error_t i2c_cmd_write(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_t data, uint8_t data_send);
+i2c_error_t i2c_cmd_read(int const i2c_port_fd, uint8_t const i2c_addr, uint8_t const cmd, uint8_t *const output, uint8_t const output_size);
 
 /**
  * @brief Send a command and receive output. E.g. Send a register address, and read its contents.
  * @param i2c_port_fd File descriptor of the i2c port.
  * @param cmd The byte that will be interpreted by the slave.
  * @param output Pointer where the output will be written. Undefined on failure.
- * @return Status code
+ * @param output_size The number of bytes that are expected to be sent back by the slave.
+ * @return Status code.
  */
-i2c_error_t i2c_cmd_read(int i2c_port_fd, uint8_t i2c_addr, uint8_t cmd, uint8_t *output);
+i2c_error_t i2c_cmd_read(int const i2c_port_fd, uint8_t const i2c_addr, uint8_t const cmd, uint8_t *const output, uint8_t const output_size);
 
 #endif /* _TCO_LIBD_I2C_H_ */
